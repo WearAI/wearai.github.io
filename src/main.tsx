9410296e49f293cc508b7e39262a6e309328676e
danielarenaa <z5361677@ad.unsw.edu.au>
2025-04-18 21:34:45 +1000

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {BrowserRouter, Routes, Route} from "react-router";
+import { HashRouter, Routes, Route } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="*" element={<App />} />
       </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+    </HashRouter>
+  </StrictMode>
 )
